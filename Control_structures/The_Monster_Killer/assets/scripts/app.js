8a4470174f9b1f34eabf3c65e2c8ebd304521a3c
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-16 17:02:21 +0530

    more function added in project

--- Control_structures/The_Monster_Killer/assets/scripts/app.js
+++ Control_structures/The_Monster_Killer/assets/scripts/app.js
@@ -1,15 +1,24 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 let chosenMaxLife = 100;
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
-
 
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler(){
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
-
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if(currentMonsterHealth <=0 && currentPlayerHealth > 0){
+    alert('YOU WON!');
+  } else if(currentPlayerHealth <= 0){
+    alert('YOU LOST');
+  } else if(currentPlayerHealth <=0 && currentMonsterHealth <=0){
+    alert('YOU HAVE A DRAW!');
+  }
 }
 
 attackBtn.addEventListener('click',attackHandler);
