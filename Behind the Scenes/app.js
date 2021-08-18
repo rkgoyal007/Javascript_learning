5d1f1bbb31cf6a39775c43687cf525dcdac00932
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-18 12:55:52 +0530

    understanding let const and var scope

---
+++ Behind the Scenes/app.js
@@ -0,0 +1,14 @@
+var name = 'Max';
+
+if(name === 'Max'){
+  var hobbies = ['music','sports'];
+  console.log(hobbies);
+}
+
+function greet(){
+  let age = 30;
+  let name = 'Manu';
+  console.log(name,age,hobbies);
+}
+console.log(name);
+greet(); 
