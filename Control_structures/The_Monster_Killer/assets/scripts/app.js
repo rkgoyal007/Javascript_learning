3e20b674617917e54a4b4b3afb2aa0189757ec60
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-18 18:10:40 +0530

    new project started - Rock, Paper, Scissor

--- Control_structures/The_Monster_Killer/assets/scripts/app.js
+++ Control_structures/The_Monster_Killer/assets/scripts/app.js
@@ -185,10 +185,7 @@ function endRound() {
 
 function attackMonster(mode) {
   const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
-  const logEvent =
-    mode === MODE_ATTACK
-      ? LOG_EVENT_PLAYER_ATTACK
-      : LOG_EVENT_PLAYER_STRONG_ATTACK;
+  const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
   // if (mode === MODE_ATTACK) {
   //   maxDamage = ATTACK_VALUE;
   //   logEvent = LOG_EVENT_PLAYER_ATTACK;
---
+++ functions/app.js
@@ -0,0 +1,8 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame(){
+  console.log('Starting game here...')
+}
+
+
+startGameBtn.addEventListener('click',startGame);
