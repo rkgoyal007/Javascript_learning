a9168c683fe17368ea0f6a62454d8cf793711ff8
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-18 13:10:05 +0530

    hoisting understand using examples

--- Behind the Scenes/app.js
+++ Behind the Scenes/app.js
@@ -1,14 +1,18 @@
-var name = 'Max';
+// var name = 'Max';
 
-if(name === 'Max'){
-  var hobbies = ['music','sports'];
-  console.log(hobbies);
-}
+// if(name === 'Max'){
+//   var hobbies = ['music','sports'];
+//   console.log(hobbies);
+// }
 
-function greet(){
-  let age = 30;
-  let name = 'Manu';
-  console.log(name,age,hobbies);
-}
+// function greet(){
+//   let age = 30;
+//   let name = 'Manu';
+//   console.log(name,age,hobbies);
+// }
+// console.log(name);
+// greet(); 
+
 console.log(name);
-greet(); 
+
+var name ='Max';
