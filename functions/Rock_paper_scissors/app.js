f1895a701d758f992efa7fd13caa2f075b04cfd7
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-19 19:32:32 +0530

    learned bind()  method

--- functions/Rock_paper_scissors/app.js
+++ functions/Rock_paper_scissors/app.js
@@ -108,3 +108,30 @@ startGameBtn.addEventListener('click', () => {
 // };
 
 // sumUp(showResult,2,3,4,5,5); 
+
+
+
+//Bind()
+
+// const combine = (resultHandler, operation, ...numbers) => {
+//   const validateNumber = number => {
+//     return isNaN(number) ? 0 : number;
+//   };
+//   let sum = 0;
+//   for (const num of numbers) {
+//     if (operation === 'ADD') {
+//       sum += validateNumber(num);
+//     } else {
+//       sum -= validateNumber(num);
+//     }
+//   }
+//   resultHandler(sum);
+// };
+
+// const showResult = (messageText, result) => {
+//   alert(messageText + ' ' + result);
+// };
+
+// combine(showResult.bind(this, 'The result after adding all numbers is:'),'ADD',1, 5,-3,6,10);
+// combine(showResult.bind(this, 'The result after adding all numbers is:'),'ADD', 1, 5,10,-3,6,);
+// combine(showResult.bind(this, 'The result after subtracting all numbers is:'),'SUBTRACT',1,10,15,20);
