1a05092318980bbbf1745e6cb7a05febad9b96d7
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-19 14:31:46 +0530

    understand default argument behaviour

--- functions/Rock_paper_scissors/app.js
+++ functions/Rock_paper_scissors/app.js
@@ -2,67 +2,74 @@ const startGameBtn = document.getElementById('start-game-btn');
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
-const SCISSOR = 'SCISSOR';
+const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
-
 const RESULT_DRAW = 'DRAW';
 const RESULT_PLAYER_WINS = 'PLAYER_WINS';
 const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
-let gameIsRunning = false; 
+let gameIsRunning = false;
 
 const getPlayerChoice = () => {
-  const selection = prompt(`${ROCK},${PAPER} or ${SCISSOR} ?`,'').toUpperCase();
-  if(selection !== ROCK   && selection !== PAPER && selection !== SCISSOR){
-    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you`);
-    return DEFAULT_USER_CHOICE;
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`,'').toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return;
   }
   return selection;
 };
 
 const getComputerChoice = () => {
   const randomValue = Math.random();
-  if(randomValue<0.34){
+  if (randomValue < 0.34) {
     return ROCK;
-  } else if(randomValue<0.67){
+  } else if (randomValue < 0.67) {
     return PAPER;
-  } else{
-    return SCISSOR;
+  } else {
+    return SCISSORS;
   }
 };
 
-const getWinner = (cChoice,pChoice) => {
-  return cChoice === pChoice ? RESULT_DRAW : (cChoice === ROCK && pChoice === PAPER ||
-         cChoice === PAPER && pChoice ===SCISSOR  ||
-         cChoice === SCISSOR && pChoice === ROCK) ? RESULT_PLAYER_WINS : RESULT_COMPUTER_WINS; // Nested Ternery operator use
-  
-  // if(cChoice === pChoice){
-  //   return RESULT_DRAW;
-  // } else if(cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice ===SCISSOR  || cChoice === SCISSOR && pChoice === ROCK){
-  //   return RESULT_PLAYER_WINS;
-  // } else {
-  //   return RESULT_COMPUTER_WINS;
-  // }
-};
+const getWinner = (cChoice,pChoice = DEFAULT_USER_CHOICE) =>{               //Default argument
+  cChoice === pChoice ? RESULT_DRAW: (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) || (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS : RESULT_COMPUTER_WINS;}
 
-startGameBtn.addEventListener('click',() => {
-  if(gameIsRunning){
+// if (cChoice === pChoice) {
+//   return RESULT_DRAW;
+// } else if (
+//   (cChoice === ROCK && pChoice === PAPER) ||
+//   (cChoice === PAPER && pChoice === SCISSORS) ||
+//   (cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+//   return RESULT_PLAYER_WINS;
+// } else {
+//   return RESULT_COMPUTER_WINS;
+// }
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
-  console.log('Starting game here...');
+  console.log('Game is starting...');
   const playerChoice = getPlayerChoice();
-  const computerChoice =  getComputerChoice();
-  const winner = getWinner(computerChoice,playerChoice);
-  let message = `You picked ${playerChoice},computer picked ${computerChoice} therefore you ` ;
-  if(winner === RESULT_DRAW){
-    message +=  `had a draw`;
-  } else if(winner === RESULT_PLAYER_WINS){
-    message += `won`;
+  const computerChoice = getComputerChoice();
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
   } else {
-    message += `lost`;
+    winner = getWinner(computerChoice);
+  }
+  let message = `You picked ${playerChoice ||
+    DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won.';
+  } else {
+    message = message + 'lost.';
   }
   alert(message);
   gameIsRunning = false;
 });
-
