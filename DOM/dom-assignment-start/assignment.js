8b1bff4e62a86d045aa95bc2756bb13f45545353
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-20 17:45:19 +0530

    assignment on DOM added

---
+++ DOM/dom-assignment-start/assignment.js
@@ -0,0 +1,10 @@
+const id = document.getElementById('task-1');
+const id2 = document.querySelector('#task-1');
+id2.style.color= "white";
+id2.style.backgroundColor= "black";
+
+const title1 = document.querySelector('title');
+title1.textContent = 'Assignment-Solved!';
+
+const head = document.querySelector('h1');
+head.textContent = 'Assignment-Solved!';
