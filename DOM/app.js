57def3ef449952c51952b5e20f972b74f27fc5f4
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-20 19:48:20 +0530

    learned traversing childNode and parentNode

--- DOM/app.js
+++ DOM/app.js
@@ -1,16 +1,30 @@
 // const listItemElememts = document.querySelectorAll('li');
-const listItemElememts = document.getElementsByTagName('li');
+// const listItemElememts = document.getElementsByTagName('li');
 
-const body = document.body;
+// const body = document.body;
 
-const li = document.querySelector('li:last-of-type');  // sudo selector 
-li.textContent = li.textContent +('changed!');
+// const li = document.querySelector('li:last-of-type');  // sudo selector 
+// li.textContent = li.textContent +('changed!');
 
-const h1 = document.getElementById('main-title');
-h1.textContent = 'some new title1';
-h1.style.color = "white";
-h1.style.backgroundColor = "red";
+// const h1 = document.getElementById('main-title');
+// h1.textContent = 'some new title1';
+// h1.style.color = "white";
+// h1.style.backgroundColor = "red";
 
- for(let listItemEle of listItemElememts){
-   console.dir(listItemEle);
- }
+//  for(let listItemEle of listItemElememts){
+//    console.dir(listItemEle);
+//  }
+
+//understanding child node concept
+// const list = document.querySelector('ul');
+// console.log(list.children);   // 3 child element nodes
+// console.log(list.children[1]); // 2nd child element node
+// console.log(list.childNodes);  // 7 child nodes
+
+//understanding parent node concept
+// const liFirst = document.querySelector('li');
+// liFirst.parentNode              // shows parent Node
+// liFirst.closest('body');        // accessing anncestor
+
+
+
