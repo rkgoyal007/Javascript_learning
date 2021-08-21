eba6b96dc359b9aa8dbff54b8b4839a2bccc6d45
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-21 19:56:51 +0530

    backdrop and cancel button functionality added

--- DOM/Movie_Project/assets/scripts/app.js
+++ DOM/Movie_Project/assets/scripts/app.js
@@ -2,9 +2,28 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
-const toggleMovieModal = ()=>{
-  addMovieModal.classList.toggle('visible');
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
 };
-startAddMovieButton.addEventListener('click',toggleMovieModal);
 
+const toggleMovieModal = () => { // function() {}
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
