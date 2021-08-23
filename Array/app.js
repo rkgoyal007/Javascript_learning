f486b11d14bed2703fe2a074132b9f461b6a6bf1
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-23 20:10:36 +0530

    learned use of slice method on arrays

--- Array/app.js
+++ Array/app.js
@@ -17,13 +17,26 @@
 // const arrayListItems = Array.from(listItems);
 // console.log(arrayListItems);
 
-const personalData = [30,'max',{moreDetails : []}];
+// const personalData = [30,'max',{moreDetails : []}];
 
-const analyticsData = [[1,1.6],[-5.4,2.1]]
-console.log(personalData[1]);
+// const analyticsData = [[1,1.6],[-5.4,2.1]]
+// console.log(personalData[1]);
 
-for(const data of analyticsData){
-  for(const daatPoints of data){
-    console.log(daatPoints);
-  }
-}
+// for(const data of analyticsData){
+//   for(const daatPoints of data){
+//     console.log(daatPoints);
+//   }
+// }
+
+const hobbies = ['sports','cooking'];
+hobbies.push('reading');
+hobbies.unshift('coding');
+hobbies.splice(0,0,'good food');     // inserted 1 element at index 0
+console.log(hobbies);
+hobbies.splice(1,2);                // delete 2 element at index 1
+console.log(hobbies);
+
+const removedElements = hobbies.splice(2,1);
+
+hobbies.splice(-1,1);              // delete last indexed element
+console.log(hobbies);
