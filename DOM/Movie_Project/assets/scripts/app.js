c1c3df5c4aae614c17bca51fe932d6aba121535f
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-23 13:45:10 +0530

    rendered movie element

--- DOM/Movie_Project/assets/scripts/app.js
+++ DOM/Movie_Project/assets/scripts/app.js
@@ -9,8 +9,33 @@ const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementsByTagName('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () =>{
+  if(movies.length === 0){
+    entryTextSection.style.display = 'block';
+  } else{
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
 
 
 const toggleBackdrop = () => {
@@ -43,15 +68,17 @@ const addMovieHandler = () => {
     alert('Please enter valid values (rating between 1 and 5).');
     return;
   }
-  const newMovies = {
+  const newMovie = {
     title : titleValue,
     image : imageUrlValue,
     rating : ratingValue
   };
-  movies.push(newMovies);
+  movies.push(newMovie);
   console.log(movies);
   toggleMovieModal();
   clearMovieInput();
+  renderNewMovieElement(newMovie.title,newMovie.image,newMovie.rating);
+  updateUI();
 };
 
 const backdropClickHandler = () => {
