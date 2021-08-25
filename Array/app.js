d66221c00bb15808d225bc5ea24b6796cbdff50a
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-25 13:58:06 +0530

    array destructuring learned

--- Array/app.js
+++ Array/app.js
@@ -135,16 +135,20 @@
 // console.log(name);
 
 // Soread operator
-const nameFragments = ['rahul','goyal'];
-const copiedNameFragments = [...nameFragments];
-console.log(nameFragments,copiedNameFragments);
+// const nameFragments = ['rahul','goyal'];
+// const copiedNameFragments = [...nameFragments];
+// console.log(nameFragments,copiedNameFragments);
 
-const prices = [4,5,6,7,8,5,6];
-const minNumber = Math.min(...prices);
-console.log(minNumber);
+// const prices = [4,5,6,7,8,5,6];
+// const minNumber = Math.min(...prices);
+// console.log(minNumber);
 
-const persons = [{name:'max', age:34 },{name: 'rahul', age:24}];
+// const persons = [{name:'max', age:34 },{name: 'rahul', age:24}];
 //const copiedPersons = [...persons];                         //it just copy the reference of objects
-const copiedPersons = persons.map(person => ({name: person.name,age: person.age}));
-console.log(copiedPersons);
+// const copiedPersons = persons.map(person => ({name: person.name,age: person.age}));
+// console.log(copiedPersons);
 
+//Array Destructuring
+const nameData = ['rahul','goyal','neha',5,78];
+const [firstName,lastName,...otherInfo] = nameData;
+console.log(firstName,lastName,otherInfo);
