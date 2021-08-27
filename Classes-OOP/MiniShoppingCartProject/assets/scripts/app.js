15552b7a933f4a4fbeb28a004c765ebe4193d9e2
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-27 16:32:34 +0530

    class and its constructor created and class object created

---
+++ Classes-OOP/MiniShoppingCartProject/assets/scripts/app.js
@@ -0,0 +1,54 @@
+class Product {
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  } 
+}
+
+const productList = {
+  products: [
+    new Product(
+      'A Pillow',
+      'https://wakefit-co.s3.ap-south-1.amazonaws.com/img/pillow-cushions/BG/2.jpg',
+      'A soft pillow!',
+      19.99
+    ),
+    new Product(
+      'A Carpet',
+      'https://cdn.w600.comps.canstockphoto.com/carpet-stock-photograph_csp2914899.jpg ',
+      'A carpet which you might like - or not.',
+      89.99
+    )
+  ],
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
+        <div>
+          <img src="${prod.imageUrl}" alt="${prod.title}" >
+          <div class="product-item__content">
+            <h2>${prod.title}</h2>
+            <h3>\$${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+};
+
+productList.render();
