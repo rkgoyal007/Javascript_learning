0955ec4af7b99026163300e6dae8a269f95cc3b9
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-08-27 17:24:01 +0530

    improved class code further

--- Classes-OOP/MiniShoppingCartProject/assets/scripts/app.js
+++ Classes-OOP/MiniShoppingCartProject/assets/scripts/app.js
@@ -12,6 +12,30 @@ class Product {
   } 
 }
 
+class ProductItem {
+  constructor(product){
+    this.product = product;
+  }
+
+  render(){
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+
+  }
+}
+
 class ProductList {
   products = [
     new Product(
@@ -35,19 +59,8 @@ class ProductList {
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}" >
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
