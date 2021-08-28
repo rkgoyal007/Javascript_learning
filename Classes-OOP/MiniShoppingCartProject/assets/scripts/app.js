d092300ad68b7b711879b861170a521b14ae2197
Rahul Goyal <rkgoyal007@gmail.com>
2021-08-28 17:18:03 +0530

    learned static methods in class

--- Classes-OOP/MiniShoppingCartProject/assets/scripts/app.js
+++ Classes-OOP/MiniShoppingCartProject/assets/scripts/app.js
@@ -9,52 +9,55 @@ class Product {
     this.imageUrl = image;
     this.description = desc;
     this.price = price;
-  } 
+  }
 }
 
-class ShoppingCart{
+class ShoppingCart {
   items = [];
-  
-  render(){
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
     const cartEl = document.createElement('section');
     cartEl.innerHTML = `
-    <h2>Total: \$${0}</h2>
-    <button>Order Now</button>
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
     `;
-    cartEl.classList= 'cart';
+    cartEl.className = 'cart';
+    this.totalOutput = cartEl.querySelector('h2');
     return cartEl;
   }
 }
 
 class ProductItem {
-  constructor(product){
+  constructor(product) {
     this.product = product;
   }
 
-  addToCart(){
-    console.log('Adding product to cart...'); 
-    console.log(this.product);
-
+  addToCart() {
+    App.addProductToCart(this.product);
   }
 
-  render(){
+  render() {
     const prodEl = document.createElement('li');
     prodEl.className = 'product-item';
     prodEl.innerHTML = `
-      <div>
-        <img src="${this.product.imageUrl}" alt="${this.product.title}" >
-        <div class="product-item__content">
-          <h2>${this.product.title}</h2>
-          <h3>\$${this.product.price}</h3>
-          <p>${this.product.description}</p>
-          <button>Add to Cart</button>
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
         </div>
-      </div>
-    `;
+      `;
     const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click',this.addToCart.bind(this));
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
     return prodEl;
-
   }
 }
 
@@ -62,19 +65,19 @@ class ProductList {
   products = [
     new Product(
       'A Pillow',
-      'https://wakefit-co.s3.ap-south-1.amazonaws.com/img/pillow-cushions/BG/2.jpg',
+      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
       'A soft pillow!',
       19.99
     ),
     new Product(
       'A Carpet',
-      'https://cdn.w600.comps.canstockphoto.com/carpet-stock-photograph_csp2914899.jpg ',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
       'A carpet which you might like - or not.',
       89.99
     )
   ];
 
-  constructor() {};
+  constructor() {}
 
   render() {
     const prodList = document.createElement('ul');
@@ -86,20 +89,37 @@ class ProductList {
     }
     return prodList;
   }
-};
+}
 
-class Shop{
-  render(){
-  const renderHook = document.getElementById('app');
-  const cart = new ShoppingCart();
-  const cartEl = cart.render();
-  const productList = new ProductList();
-  const prodListEl = productList.render();
+class Shop {
+  
+  render() {
+    const renderHook = document.getElementById('app');
 
-  renderHook.append(cartEl);
-  renderHook.append(prodListEl);
-}}
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
 
-const shop = new Shop();
-shop.render(); 
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
+
 
