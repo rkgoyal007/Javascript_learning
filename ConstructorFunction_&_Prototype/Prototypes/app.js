6970740802941afc3c8f9836ca3e96f158029668
Rahul Goyal <rkgoyal007@gmail.com>
2021-08-31 21:02:33 +0530

    learned getting and setting prototypes

--- ConstructorFunction_&_Prototype/Prototypes/app.js
+++ ConstructorFunction_&_Prototype/Prototypes/app.js
@@ -65,11 +65,54 @@ class Person extends AgedPerson{
 // console.dir(Object.prototype);
 // console.dir(Object.prototype.__proto__);
 
-const p = new Person();
-const p2 = new Person();
-//console.log(p.__proto__ === p2.__proto__);
-p.greet();
-console.log(p);
+// const p = new Person();
+// const p2 = new Person();
+// //console.log(p.__proto__ === p2.__proto__);
+// p.greet();
+// console.log(p);
 
-const button = document.getElementById('btn');
-button.addEventListener('click',p.greet.bind(p));
+// const button = document.getElementById('btn');
+// button.addEventListener('click',p.greet.bind(p));
+
+
+const course = {
+  // new Object()
+  title: 'JavaScript - The Complete Guide',
+  rating: 5
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function() {
+    console.log(`${this.rating}/5`);
+  }
+});
+
+const student = Object.create({
+  printProgress: function() {
+    console.log(this.progress);
+  }
+}, {
+  name: {
+    configurable: true,
+    enumerable: true,
+    value: 'Max',
+    writable: true
+  }
+});
+
+// student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
