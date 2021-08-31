14aa4afb1e7743338e4ac886f2b3e065ec4c0351
Rahul Goyal <rkgoyal007@gmail.com>
2021-08-31 19:26:22 +0530

    rearranged some code and learned prototype chaining

--- ConstructorFunction_&_Prototype/Prototypes/app.js
+++ ConstructorFunction_&_Prototype/Prototypes/app.js
@@ -1,23 +1,3 @@
-//functional way of doing it
-
-function Person()
-{
-  this.age = 30;
-  this.name = 'Max';
-  this.greet = function(){
-    console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
-  };
-}
-
-// Person.prototype = {                              // creating a new prototype
-//   printAge(){
-//     console.log(this.age);
-//   }
-// }
-Person.prototype.printAge = function(){               // adding new property to existing prototype(typically do this to remain having constructor method)
-  console.log(this.age);
-};
-
 // class way of doing it
 
 // class AgedPerson{
@@ -40,12 +20,43 @@ Person.prototype.printAge = function(){               // adding new property to 
 // }
 
 
+//functional way of doing it
+
+function Person()
+{
+  this.age = 30;
+  this.name = 'Max';
+  this.greet = function(){
+    console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
+  };
+}
+
+Person.describe = function(){
+  console.log('Creating persons...');
+}
+
+// Person.prototype = {                              // creating a new prototype
+//   printAge(){
+//     console.log(this.age);
+//   }
+// }
+
+Person.prototype.printAge = function(){               // adding new property to existing prototype(typically do this to remain having constructor method)
+  console.log(this.age);
+};
+
+console.log(Person);
+
 const p = new Person; 
 p.greet();
 p.printAge();
 //console.log(p.toString());
 console.log(p.__proto__ === Person.prototype);
-console.log(p.__proto__);
-
+console.log(p);
+console.log(p.legth);
+ 
 const p2 = new p.__proto__.constructor;
 console.log(p2);
+
+console.dir(Object.prototype);
+console.dir(Object.prototype.__proto__);
