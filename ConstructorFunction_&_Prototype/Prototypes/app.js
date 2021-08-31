967bff6e52c9ca4fd4ef90214da9104fd734b290
Rahul Goyal <rkgoyal007@gmail.com>
2021-08-31 20:53:00 +0530

    different methods on classes and constructors

--- ConstructorFunction_&_Prototype/Prototypes/app.js
+++ ConstructorFunction_&_Prototype/Prototypes/app.js
@@ -1,39 +1,45 @@
-// class way of doing it
+//class way of doing it
 
-// class AgedPerson{
-//   printAge(){
-//     console.log(this.age);
-//   }
-// }
+class AgedPerson{
+  printAge(){
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson{
-//   name = 'Max';
+class Person extends AgedPerson{
+  name = 'Max';
 
-//   constructor(){
-//     super();
-//     this.age = 30;
-//   }
+  constructor(){
+    super();
+    this.age = 30;
+    // this.greet()....};
+  }
 
-//    greet(){
-//     console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
-//   }
-// }
+  // greet = () => {
+  //   console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
+  // };
 
-
-//functional way of doing it
-
-function Person()
-{
-  this.age = 30;
-  this.name = 'Max';
-  this.greet = function(){
+   greet(){
     console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
   };
 }
 
-Person.describe = function(){
-  console.log('Creating persons...');
-}
+
+//functional way of doing it(constructor function)
+
+// function Person()
+// {
+//   this.age = 30;
+//   this.name = 'Max';
+//   // this.greet(){....};
+// }
+// Person.prototype.greet = function(){
+//   console.log(`Hi,I am  ${this.name} and I am ${this.age} years old`);
+// };
+
+// Person.describe = function(){
+//   console.log('Creating persons...');
+// }
 
 // Person.prototype = {                              // creating a new prototype
 //   printAge(){
@@ -41,22 +47,29 @@ Person.describe = function(){
 //   }
 // }
 
-Person.prototype.printAge = function(){               // adding new property to existing prototype(typically do this to remain having constructor method)
-  console.log(this.age);
-};
+// Person.prototype.printAge = function(){               // adding new property to existing prototype(typically do this to remain having constructor method)
+//   console.log(this.age);
+// };
 
-console.log(Person);
-
-const p = new Person; 
-p.greet();
-p.printAge();
-//console.log(p.toString());
-console.log(p.__proto__ === Person.prototype);
-console.log(p);
-console.log(p.legth);
+// const p = new Person; 
+// p.greet();
+// p.printAge();
+// //console.log(p.toString());
+// console.log(p.__proto__ === Person.prototype);
+// console.log(p);
+// console.log(p.legth);
  
-const p2 = new p.__proto__.constructor;
-console.log(p2);
+// const p2 = new p.__proto__.constructor;
+// console.log(p2);
 
-console.dir(Object.prototype);
-console.dir(Object.prototype.__proto__);
+// console.dir(Object.prototype);
+// console.dir(Object.prototype.__proto__);
+
+const p = new Person();
+const p2 = new Person();
+//console.log(p.__proto__ === p2.__proto__);
+p.greet();
+console.log(p);
+
+const button = document.getElementById('btn');
+button.addEventListener('click',p.greet.bind(p));
