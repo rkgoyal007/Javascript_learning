8f79269ab607dd9aa4d9b0fa69de95bc6362dd93
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-06 14:06:47 +0530

    learned about pure and impure function

--- AdvancedFunctionConcept/app.js
+++ AdvancedFunctionConcept/app.js
@@ -1,5 +1,28 @@
-function add(num1,num2){
+function add(num1,num2){            //pure function
     return num1+num2;
 }
 
 console.log(add(2,6));
+console.log(add(2,246));
+
+function random (num1){             // Impure function
+    return num1+Math.random();
+}
+
+console.log(random(7));
+
+let previousResult = 0;
+function addMore(num1,num2){         // Impure function
+    let sum=num1+num2;
+    previousResult =sum;
+    return  sum;
+}
+addMore(5,9);
+console.log(previousResult);
+
+const hobbie = ['cooking','sports'];
+function showHobbies(h){             // Impure function
+    h.push('biking'); 
+    console.log(h);
+}
+showHobbies(hobbie);
