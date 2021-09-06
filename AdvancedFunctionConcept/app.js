60bad6fdf6da7c4e2c79aa69cbb0a2376771b2a2
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-06 14:27:41 +0530

    learned about factory function

--- AdvancedFunctionConcept/app.js
+++ AdvancedFunctionConcept/app.js
@@ -26,3 +26,17 @@ function showHobbies(h){             // Impure function
     console.log(h);
 }
 showHobbies(hobbie);
+
+function createTaxCalculator(tax){             // factory function
+    function calculateTax(amount){
+        return amount*tax;
+    }
+    return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
+
