0984e6ab8fc0cd8cf8d9a339dcc3a6b10a6d6863
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-07 15:57:48 +0530

    Tagged template code added

--- More_on_numbers_&_Strings/app.js
+++ More_on_numbers_&_Strings/app.js
@@ -1,8 +1,27 @@
 // function to get random number btw min and max
 
-function randomIntBetween(min,max){                             // let min:5 and max:10
-  return Math.floor(Math.random() * (max - min + 1) + min);    // 10.99999
+function randomIntBetween(min,max){                              // let min:5 and max:10
+  return Math.floor(Math.random() * (max - min + 1) + min);      // 10.99999
 
 }
-
 console.log(randomIntBetween(1,10));
+
+//Tagged template
+
+function productDescription(strings,productName,productPrice){     
+  console.log(strings);                                           //first argument is array of strings(non dynamic string part)
+  console.log(productName);
+  console.log(productPrice);
+  let priceCatagory = 'very cheap regarding its price';
+  if(productPrice>20){
+    priceCatagory = 'fairly priced';
+  }
+  //return `${strings[0]} ${productName} ${strings[1]} ${priceCatagory} ${strings[2]}`;
+  return {name : productName , price : prodPrice};                // returns object with dynamic data
+}
+
+const prodName = 'javaScript Course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is (${prodPrice}). `;
+console.log(productOutput);
