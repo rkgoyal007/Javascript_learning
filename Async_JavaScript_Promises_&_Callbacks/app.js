67aebcb53eb08ee04b8ee2b6b22f6ea69e2518dd
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-08 12:56:28 +0530

    understanded sync and async code execution

--- Async_JavaScript_Promises_&_Callbacks/app.js
+++ Async_JavaScript_Promises_&_Callbacks/app.js
@@ -2,12 +2,18 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(posData =>{
+    console.log(posData)
+  },error =>{
+    console.log(error);
+  },
+ ); 
+ console.log('Getting position...');
 }
+ button.addEventListener('click', trackUserHandler);
 
-button.addEventListener('click', trackUserHandler);
-let result = 0;
-for(let i = 0;i < 100000000;i++){
-  result+=i;
-}
-console.log(result);
+ // let result = 0;
+// for(let i = 0;i < 100000000;i++){
+//   result+=i;
+// }
+// console.log(result);
