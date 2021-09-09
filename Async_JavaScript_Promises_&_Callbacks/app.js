63c1619cc4feee33fabc36eb9403d7d59f6acadc
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-09 18:39:25 +0530

    learned async-await

--- Async_JavaScript_Promises_&_Callbacks/app.js
+++ Async_JavaScript_Promises_&_Callbacks/app.js
@@ -12,7 +12,7 @@ const getPosition = (opts) => {
   return promise;
 };
 
-const setTimer = (duration) => {
+const setTimer = async duration => {
   const promise = new Promise((resolve, reject) => {  
     setTimeout(() => {
       resolve("Done!");
@@ -21,25 +21,26 @@ const setTimer = (duration) => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()  
-  .then(posData =>{
-    positionData = posData;
-    return setTimer(2000);
-    })
-    .catch(err => {
-      console.log(err);
-      return 'on we go...';
-    })   
-    .then((data)=>{
-      console.log(data,positionData);
-    });
-  setTimer(1000).then(() => {
-    console.log("Timer Done!");
-  })
-
-  console.log("Getting position...");
+async function trackUserHandler() {
+  //let positionData;
+  const posData = await getPosition();
+  const timerData = await setTimer(2000)
+  console.log(timerData,posData);
+  // .then(posData =>{
+  //   positionData = posData;
+  //   return setTimer(2000);
+  //   })
+  // .catch(err => {
+  //   console.log(err);
+  //   return 'on we go...';
+  //  })   
+  // .then((data)=>{
+  //   console.log(data,positionData);
+  // });
+  // setTimer(1000).then(() => {
+  //   console.log("Timer Done!");
+  // })
+  // console.log("Getting position...");
 }
 
 button.addEventListener("click", trackUserHandler);
