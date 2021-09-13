b0fe9e6de275317d4ae783f76e16e6e31402dfe4
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-13 21:50:02 +0530

    added code of sending a delete request

--- HttpRequest/assets/scripts/app.js
+++ HttpRequest/assets/scripts/app.js
@@ -1,7 +1,8 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button')
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url,data ) {
   const promise = new Promise((resolve, reject) => {
@@ -27,6 +28,7 @@ async function fetchPosts(){
       const postEl = document.importNode(postTemplate.content, true);
       postEl.querySelector("h2").textContent = post.title.toUpperCase();
       postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector('li').id = post.id;
       listElement.append(postEl);
     }
 }
@@ -47,4 +49,11 @@ form.addEventListener('submit',event =>{
   const enteredTitle = event.currentTarget.querySelector('#title').value;
   const enteredContent = event.currentTarget.querySelector('#content').value;
   createPost(enteredTitle,enteredContent);
-})
+});
+
+postList.addEventListener('click',event =>{
+  if(event.target.tagName === 'BUTTON'){
+    const postId = event.target.closest('li').id;
+    sendHttpRequest('DELETE',`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  }
+});
