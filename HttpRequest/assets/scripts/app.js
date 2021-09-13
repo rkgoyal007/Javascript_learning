2a9a00a37bd5e113e0cdf671637d32e5a6163505
Rahul Goyal <rkgoyal007@gmail.com>
2021-09-13 20:22:43 +0530

    code of Sending data with Post request is added

--- HttpRequest/assets/scripts/app.js
+++ HttpRequest/assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url,data ) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -13,7 +13,7 @@ function sendHttpRequest(method, url) {
       //const listOfPost = JSON.parse(xhr.response);
       resolve(xhr.response);
     };
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
@@ -28,4 +28,16 @@ async function fetchPosts(){
       listElement.append(postEl);
     }
 }
+
+async function createPost(title,content){
+  const userId = Math.random();
+  const post = {
+    title : title,
+    body : content,
+    userId : userId
+  }
+  sendHttpRequest('POST','https://jsonplaceholder.typicode.com/posts',post);
+}
+
 fetchPosts();
+createPost('DUMMY','A dummy post!');
