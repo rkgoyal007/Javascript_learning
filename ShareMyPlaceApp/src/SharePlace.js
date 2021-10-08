b261f06f76bfe290b371fd37545ea7a666d03629
Rahul Goyal <rkgoyal007@gmail.com>
2021-10-08 15:53:09 +0530

    code added for getting the user location

--- ShareMyPlaceApp/src/SharePlace.js
+++ ShareMyPlaceApp/src/SharePlace.js
@@ -8,10 +8,24 @@ class PlaceFinder{
     }
 
     locateUserHandler(){
-
+        if(!navigator.geolocation){
+            alert(' Location feature is not available in your browser - please use a more morden Browser or manually enter an address.');
+            return;
+        }
+        navigator.geolocation.getCurrentPosition(succressResult =>{
+            const coordinates = {
+                lat: succressResult.coords.latitude + Math.random() * 50,
+                lng: succressResult.coords.longitude + Math.random() * 50
+            };
+            console.log(coordinates);
+        }, error =>{
+            alert('Could not locate you unfortunately. please enter an address manually');
+        });
     }
 
     findAddressHandler(){
 
     }
 } 
+
+const placeFinder = new PlaceFinder();
