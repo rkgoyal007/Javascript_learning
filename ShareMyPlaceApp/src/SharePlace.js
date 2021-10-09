cf066c907a6880ff763f14b7e0c0548dcfe44f86
Rahul Goyal <rkgoyal007@gmail.com>
2021-10-09 12:50:13 +0530

    code added for showing modal

--- ShareMyPlaceApp/src/SharePlace.js
+++ ShareMyPlaceApp/src/SharePlace.js
@@ -1,31 +1,42 @@
-class PlaceFinder{
-    constructor(){
-        const addressForm = document.querySelector('form');
-        const locateUserBtn = document.getElementById('locate-btn');
+import { Modal } from './UI/Modal';
 
-        locateUserBtn.addEventListener('click',this.locateUserHandler);
-        addressForm.addEventListener('submit',this.findAddressHandler);
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+      );
+      return;
     }
+    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      successResult => {
+        modal.hide();
+        const coordinates = {
+          lat: successResult.coords.latitude + Math.random() * 50,
+          lng: successResult.coords.longitude + Math.random() * 50,
+        };
+        console.log(coordinates);
+      },
+      error => {
+        modal.hide();
+        alert(
+          'Could not locate you unfortunately. Please enter an address manually!'
+        );
+      }
+    );
+  }
 
-    locateUserHandler(){
-        if(!navigator.geolocation){
-            alert(' Location feature is not available in your browser - please use a more morden Browser or manually enter an address.');
-            return;
-        }
-        navigator.geolocation.getCurrentPosition(succressResult =>{
-            const coordinates = {
-                lat: succressResult.coords.latitude + Math.random() * 50,
-                lng: succressResult.coords.longitude + Math.random() * 50
-            };
-            console.log(coordinates);
-        }, error =>{
-            alert('Could not locate you unfortunately. please enter an address manually');
-        });
-    }
-
-    findAddressHandler(){
-
-    }
-} 
+  findAddressHandler() {}
+}
 
 const placeFinder = new PlaceFinder();
