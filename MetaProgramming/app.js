b082e27b40e83e93c64837fa15d12f13a77283bc
Rahul Goyal <rkgoyal007@gmail.com>
2021-10-13 14:43:11 +0530

    learned about inerators

--- MetaProgramming/app.js
+++ MetaProgramming/app.js
@@ -7,7 +7,7 @@ const user = {
   [uid]: 'p1',
   name: 'Max',
   age: 30,
-  [Symbol.toStringTag]: 'User'
+  [Symbol.toStringTag]: 'User' 
 };
 
 user[uid] = 'p3';
@@ -19,3 +19,26 @@ user.id = 'p2'; // this should not be possible!
 console.log(user[Symbol('uid')]);
 console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString());
+
+
+const company ={
+  curEmployee : 0,
+  employees : ['Max','Manuel','Anna'], 
+  next(){
+    if(this.curEmployee >= this.employees.length){
+      return {value: this.curEmployee , done:true};
+    }
+    const returnValue =  {
+      value: this.employees[this.curEmployee],
+      done: false
+    };
+    this.curEmployee++;
+    return returnValue;
+  }
+}
+
+let employee = company.next();
+while(!employee.done){
+  console.log(employee.value);
+  employee = company.next();
+}
