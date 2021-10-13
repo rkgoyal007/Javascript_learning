43917122f711028d2712be940f5ec7c2d53429d8
Rahul Goyal <rkgoyal007@gmail.com>
2021-10-13 16:31:16 +0530

    learned about generators

--- MetaProgramming/app.js
+++ MetaProgramming/app.js
@@ -7,7 +7,7 @@ const user = {
   [uid]: 'p1',
   name: 'Max',
   age: 30,
-  [Symbol.toStringTag]: 'User' 
+  [Symbol.toStringTag]: 'User'
 };
 
 user[uid] = 'p3';
@@ -20,25 +20,52 @@ console.log(user[Symbol('uid')]);
 console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString());
 
+const company = {
+  // curEmployee: 0,
+  employees: ['Max', 'Manu', 'Anna'],
+  // next() {
+  //   if (this.curEmployee >= this.employees.length) {
+  //     return { value: this.curEmployee, done: true };
+  //   }
+  //   const returnValue = {
+  //     value: this.employees[this.curEmployee],
+  //     done: false
+  //   };
+  //   this.curEmployee++;
+  //   return returnValue;
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
 
-const company ={
-  curEmployee : 0,
-  employees : ['Max','Manuel','Anna'], 
-  next(){
-    if(this.curEmployee >= this.employees.length){
-      return {value: this.curEmployee , done:true};
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
     }
-    const returnValue =  {
-      value: this.employees[this.curEmployee],
-      done: false
-    };
-    this.curEmployee++;
-    return returnValue;
   }
+};
+
+// let employee = company.next();
+
+// while(!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+for (const employee of company) {
+  console.log(employee);
 }
 
-let employee = company.next();
-while(!employee.done){
-  console.log(employee.value);
-  employee = company.next();
-}
+console.log([...company]);
+
+// const it = company.getEmployee();
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
